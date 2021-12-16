351094766aeed1d76e6ed41c4c2672356b804fff
LilianaRabacel <96265792+LilianaRabacel@users.noreply.github.com>
2021-12-16 21:17:02 +0000

    Initial commit

---
+++ password.js
@@ -0,0 +1,21 @@
+function checkPassword() {
+    //capture the user entered password for our input element
+     var passwordBoxObject = document.getElementById("passwordBox");
+     var passwordEntered = passwordBoxObject.value;
+     console.log("User entered password:", passwordEntered); 
+
+    //check if password is correct
+    var sitePassword = "codeclub";
+    if (passwordEntered ==sitePassword) {
+        //allow navigation
+        console.log("Allow");
+        window.location.assign("Members.html");
+    } else {
+        //block navigation
+        console.log("Block");
+      //add a new style rule to the incorrect-password element
+        document.getElementById("incorrect-password"). style.color = "red";
+      //add text to the incorrect-password element
+        document.getElementById("incorrect-password").innerHTML = "Incorrect password. Please try again!"
+    }
+}
